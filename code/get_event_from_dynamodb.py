b64de2b5c3b89779794895863214068be63f1429
Geoffrey S. Young <gsy@integrichain.com>
2018-12-06 11:09:09 -0500

    add CORS headers

--- code/get_event_from_dynamodb.py
+++ code/get_event_from_dynamodb.py
@@ -32,7 +32,10 @@ def lambda_handler(event, context):
 
         return {
             "statusCode": 200, 
-            "headers": {"Content-Type": "application/json"},
+            "headers": {"Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,OPTIONS"
+            },
             "body": json.dumps(response['Item'])
         }
 
